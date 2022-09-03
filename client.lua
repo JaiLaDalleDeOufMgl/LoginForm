e998f77d42e4066b28328e841e4862e60234e28f
Bruno DelPero <54162106+Shazuub@users.noreply.github.com>
2022-09-04 01:41:59 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,23 +1,35 @@
-AddEventHandler('onClientResourceStart', function(ressource)
-    if ressource == GetCurrentResourceName() then
-        Wait(100)
-        SendNUIMessage({
-            type = "show",
-            status = true,
-        })
-        SendNUIMessage({ action = 'showbutton' })
-        SetNuiFocus(true, true)
-        ---
-        CameraConnect = CreateCam("DEFAULT_SCRIPTED_CAMERA", false)
-        SetCamCoord(CameraConnect, 606.99, 847.44, 357.91)
-        SetCamRot(CameraConnect, 0.0, 0.0, 340.22)
-        SetCamFov(CameraConnect, 45.97)
-        ShakeCam(CameraConnect, "HAND_SHAKE", 0.2)
-        SetCamActive(CameraConnect, true)
-        RenderScriptCams(true, false, 2000, true, true)
-        DisplayRadar(false)
-    end
+ESX = nil 
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+AddEventHandler('esx:playerLoaded', function()
+    CreateThread(function()
+        ESX.TriggerServerCallback('esx_skin:getPlayerSkin', function(skin)
+            if skin ~= nil then
+                StartConnect()
+            end
+        end)
+    end)
 end)
+
+
+function StartConnect()
+    SendNUIMessage({
+        type = "show",
+        status = true,
+    })
+    SendNUIMessage({ action = 'showbutton' })
+    SetNuiFocus(true, true)
+    ---
+    CameraConnect = CreateCam("DEFAULT_SCRIPTED_CAMERA", false)
+    SetCamCoord(CameraConnect, 606.99, 847.44, 357.91)
+    SetCamRot(CameraConnect, 0.0, 0.0, 340.22)
+    SetCamFov(CameraConnect, 45.97)
+    ShakeCam(CameraConnect, "HAND_SHAKE", 0.2)
+    SetCamActive(CameraConnect, true)
+    RenderScriptCams(true, false, 2000, true, true)
+    DisplayRadar(false)
+end
 
 RegisterNUICallback("button", function()
     SendNUIMessage({
